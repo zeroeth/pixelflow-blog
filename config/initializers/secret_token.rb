ee77dd8583fae5d61ecd08177d660b3c95081227
Kevin Alford <zeroeth@pixelflow.org>
2011-02-09 15:23:51 -0500

    - initial rails commit - blog scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PixelflowBlog::Application.config.secret_token = 'fa892f7c46d387782d1dbee2459a31267414dec7d7f9de20e8ab8f6f7a361fd93f13bcb106fff259ff1e98a2e679f7c63ce4bfaa34dc33724c12bc14db48bbd7'
