3df58315b3083074f638fcffd41d4d1e9efe1c52
Kevin Alford <zeroeth@pixelflow.org>
2014-01-19 16:33:37 -0600

    Use responders to minimize controller, make form views nicer

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,72 +1,51 @@
 class BlogPostsController < ApplicationController
-  # GET /blog_posts
-  # GET /blog_posts.xml
+  respond_to :html, :xml, :json
+
   def index
     @blog_posts = BlogPost.order("blog_posts.created_at DESC").paginate(:per_page => 5, :page => params[:page])
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @blog_posts }
-    end
+	respond_with @blog_posts
   end
 
-  # GET /blog_posts/1
-  # GET /blog_posts/1.xml
+
   def show
     @blog_post = BlogPost.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @blog_post }
-    end
+	respond_with @blog_post
   end
 
-  # GET /blog_posts/new
-  # GET /blog_posts/new.xml
+
   def new
     @blog_post = BlogPost.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @blog_post }
-    end
+	respond_with @blog_post
   end
 
-  # GET /blog_posts/1/edit
+
   def edit
     @blog_post = BlogPost.find(params[:id])
   end
 
-  # POST /blog_posts
-  # POST /blog_posts.xml
+
   def create
     @blog_post = BlogPost.new(params[:blog_post])
 
-    respond_to do |format|
-      if @blog_post.save
-        format.html { redirect_to(@blog_post, :notice => 'Blog post was successfully created.') }
-        format.xml  { render :xml => @blog_post, :status => :created, :location => @blog_post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @blog_post.errors, :status => :unprocessable_entity }
-      end
-    end
+	if @blog_post.save
+		flash[:notice] = 'Blog post was successfully created.'
+	end
+
+	respond_with @blog_post
   end
 
-  # PUT /blog_posts/1
-  # PUT /blog_posts/1.xml
+
   def update
     @blog_post = BlogPost.find(params[:id])
 
-    respond_to do |format|
-      if @blog_post.update_attributes(params[:blog_post])
-        format.html { redirect_to(@blog_post, :notice => 'Blog post was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @blog_post.errors, :status => :unprocessable_entity }
-      end
-    end
+    if @blog_post.update_attributes(params[:blog_post])
+		flash[:notice] = 'Blog post was successfully updated.'
+	end
+
+	respond_with @blog_post
   end
 
   # DELETE /blog_posts/1
@@ -75,9 +54,6 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(blog_posts_url) }
-      format.xml  { head :ok }
-    end
+	respond_with @blog_post
   end
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,4 @@
 class BlogPost < ActiveRecord::Base
+	validates :title, :presence => true
+	validates :body,  :presence => true
 end
