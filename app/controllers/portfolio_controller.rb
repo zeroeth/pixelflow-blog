10f3df871f8d22d65e5ed67dd1fb65024dd40061
Kevin Alford <zeroeth@pixelflow.org>
2015-11-05 15:20:20 -0800

    Project templating

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,11 @@
 class PortfolioController < ApplicationController
   def index
+    @projects = [
+      {name: "Project 1" , url: "googlygoogz", image: 'goggles.png' },
+      {name: "Project 2" , url: "googlygoogz", image: 'dither.png' },
+      {name: "Dogcat Hybrids" , url: "googlygoogz", image: 'sphere.png' },
+      {name: "Universal Cats" , url: "googlygoogz", image: 'maze.png' },
+      {name: "Blackhole Infinity" , url: "googlygoogz", image: 'block.png' }
+    ]
   end
 end
