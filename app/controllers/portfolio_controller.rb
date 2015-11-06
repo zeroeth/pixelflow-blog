bbeae0ad2ca4bf14ab07ba35b80cd7288fbfbcf9
Kevin Alford <zeroeth@pixelflow.org>
2015-11-05 16:51:21 -0800

    fix anchor overflow and added projects!

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,11 +1,14 @@
 class PortfolioController < ApplicationController
   def index
     @projects = [
-      {name: "Project 1" , url: "googlygoogz", image: 'goggles.png' },
-      {name: "Project 2" , url: "googlygoogz", image: 'dither.png' },
-      {name: "Dogcat Hybrids" , url: "googlygoogz", image: 'sphere.png' },
-      {name: "Universal Cats" , url: "googlygoogz", image: 'maze.png' },
-      {name: "Blackhole Infinity" , url: "googlygoogz", image: 'block.png' }
+      {name: 'Pixeltastic' , url: 'https://github.com/zeroeth/pixeltastic', image: 'goggles.png'},
+      {name: 'Ebauche' , url: 'https://github.com/zeroeth/ebauche', image: 'clock.png'},
+      {name: 'Square Blox' , url: 'https://github.com/zeroeth/squareblox', image: 'block.png'},
+      {name: 'Brain.json' , url: 'https://github.com/zeroeth/brain.json', image: 'brain.png'},
+      {name: 'Gratuitous Raytracing' , url: 'https://github.com/zeroeth/gratuitous_raytracing', image: 'sphere.png'},
+      {name: 'dae.rb' , url: 'https://github.com/zeroeth/daerb', image: 'magic.png'},
+      {name: 'Maze Dungeon' , url: 'https://github.com/zeroeth/maze_dungeon', image: 'maze.png'},
+      {name: 'Shiver me Dithers' , url: 'https://github.com/zeroeth/shiver_me_dithers', image: 'dither.png'}
     ]
   end
 end
